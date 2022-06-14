b543ba7dd3f66e786e547200399027ef5c097804
enragedsaturday <67085884+Enragedsaturday@users.noreply.github.com>
2022-06-14 00:28:31 -0400

    updated subject for contact form

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -1,13 +1,17 @@
 module.exports = {
   root: true,
   env: {
-    es6: true,
+    es6: false,
     node: true,
   },
   extends: [
     "eslint:recommended",
     "google",
   ],
+  parserOptions: {
+    "ecmaVersion": 2017,
+    "sourceType": "module",
+  },
   rules: {
     quotes: ["error", "double"],
   },
--- functions/index.js
+++ functions/index.js
@@ -1,46 +1,45 @@
-"use strict";
 const functions = require("firebase-functions");
 const nodemailer = require("nodemailer");
-const { convertSkypackImportMapToLockfile } = require("snowpack/lib/cjs/util");
-
+const dotenv = require("dotenv");
+dotenv.config();
 
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
 
 exports.contact = functions.https.onRequest(async (request, response) => {
-  functions.logger.info("Contact Sent", {structuredData: true});
   response.set("Access-Control-Allow-Origin", "*");
   response.ok = true;
   response.send();
   const body = JSON.parse(request.body);
+  console.log(body);
 
   const mail = async () => {
-
     const transporter = nodemailer.createTransport({
-        host: 'smtp.ethereal.email',
-        port: 587,
-        auth: {
-            user: 'sigmund.tromp66@ethereal.email',
-            pass: 'hfFyNmKDS4J3qQHu8j'
-        }
+      host: "premium63.web-hosting.com",
+      port: 465,
+      auth: {
+        user: `${process.env.EMAIL}`,
+        pass: `${process.env.KEY}`,
+      },
     });
-  
+
     // send mail with defined transport object
-    let info = await transporter.sendMail({
-      from: `<${body.email}>`, // sender address
-      to: "admin@tanukitech.dev", // list of receivers
-      subject: "TanukiTech.dev Contact", // Subject line
+    const info = await transporter.sendMail({
+      from: `<${process.env.EMAIL}>`, // sender address
+      replyTo: `${body.email}`,
+      to: `${body.to}`, // list of receivers
+      subject: `${body.subject}`, // Subject line
       text: `${body.message}`, // plain text body
     });
-  
+
     console.log("Message sent: %s", info.messageId);
     // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-  
+
     // Preview only available when sending through an Ethereal account
-    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    // console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
     // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-  }
-  
+  };
+
   mail().catch(console.error);
 });
